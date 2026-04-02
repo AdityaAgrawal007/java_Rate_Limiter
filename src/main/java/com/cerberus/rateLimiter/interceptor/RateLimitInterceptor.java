c5bd377722bd609584ea4858cd3bf0735d7d6e74
AdityaSMAgrawal <adityasmagrawal@gmail.com>
2026-04-02 22:35:13 +0530

    interceptor element completed

--- src/main/java/com/cerberus/rateLimiter/interceptor/RateLimitInterceptor.java
+++ src/main/java/com/cerberus/rateLimiter/interceptor/RateLimitInterceptor.java
@@ -1,4 +1,31 @@
 package com.cerberus.rateLimiter.interceptor;
 
-public class RateLimitInterceptor {
+import com.cerberus.rateLimiter.core.RateLimitResult;
+import com.cerberus.rateLimiter.core.RateLimiter;
+import com.cerberus.rateLimiter.extractor.KeyExtractor;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+
+public class RateLimitInterceptor implements HandlerInterceptor {
+    private final RateLimiter rateLimiter;
+    private final KeyExtractor keyExtractor;
+
+    public RateLimitInterceptor(RateLimiter rateLimiter, KeyExtractor keyExtractor) {
+        this.rateLimiter = rateLimiter;
+        this.keyExtractor = keyExtractor;
+    }
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        String clientId = keyExtractor.extractRemoteAddr(request);
+        RateLimitResult result = rateLimiter.tryAquire(clientId);
+        if(!result.accepted()){
+            response.setStatus(429);
+        }
+        response.setHeader("X-RateLimit-Remaining", String.valueOf(result.remainingTokens()));
+        response.setHeader("X-RateLimit-Reset", String.valueOf(result.resetTimestamp()));
+        return result.accepted();
+    }
 }
